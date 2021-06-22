f7c92671c6564da60b0b5d0200a8d43970570b97
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-06-22 17:30:45 -0400

    React Router - updated navbar and links

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-function RenderCampsite({campsite}) {
+function RenderCampsite({ campsite }) {
    return (
       <div className="col-md-5 m-1">
          <Card>
             <CardImg top src={campsite.image} alt={campsite.name} />
-               <CardBody>
-                  <CardTitle>{campsite.name}</CardTitle>
-                  <CardText>{campsite.description}</CardText>
-               </CardBody>
+            <CardBody>
+               <CardTitle>{campsite.name}</CardTitle>
+               <CardText>{campsite.description}</CardText>
+            </CardBody>
          </Card>
       </div>
    );
 }
-   
-function RenderComments({comments}) {
+
+function RenderComments({ comments }) {
    if (comments) {
       return (
          <div className="col-md-5 m-1">
             <h4>Comments</h4>
             {comments.map(comment => {
-               return(
+               return (
                   <div key={comment.id}>
                      <p>{comment.text}<br />
-                     -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                      </p>
                   </div>
                );
@@ -37,14 +37,14 @@ function RenderComments({comments}) {
 
 function CampsiteInfo(props) {
    if (props.campsite) {
-       return (
-           <div className="container">
-               <div className="row">
-                   <RenderCampsite campsite={props.campsite} />
-                   <RenderComments comments={props.campsite.comments} />
-               </div>
-           </div>
-       );
+      return (
+         <div className="container">
+            <div className="row">
+               <RenderCampsite campsite={props.campsite} />
+               <RenderComments comments={props.campsite.comments} />
+            </div>
+         </div>
+      );
    }
    return <div />;
 };
