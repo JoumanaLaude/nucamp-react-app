39341f61727210ced2831b62bb79c1d8ca6490f1
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-06-22 16:37:47 -0400

    Header and Footer components & CSS changed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-original-surfer';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
