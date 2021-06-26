269b7780db6127fd1d2074e795f9a956014d5d34
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-06-26 12:00:29 -0400

    Week 3 Workshop

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -35,6 +36,12 @@ class Main extends Component {
             );
         }
 
+        const AboutUs = () => {
+            return (
+                <About partners={this.state.partners} />
+            );
+        }
+
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
@@ -52,6 +59,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => 
                     <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/aboutus' component={AboutUs} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                     />} />
