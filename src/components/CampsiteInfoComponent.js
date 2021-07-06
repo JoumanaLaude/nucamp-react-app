98a95bb5f938595045837bed0172c4cf57115f29
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-07-06 17:34:56 -0400

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -127,6 +128,27 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+   if (props.isLoading) {
+      return (
+         <div className="container">
+            <div className="row">
+               <Loading />
+            </div>
+         </div>
+      );
+   }
+   if (props.errMess) {
+      return (
+         <div className="container">
+            <div className="row">
+               <div className="col">
+                  <h4>{props.errMess}</h4>
+               </div>
+            </div>
+         </div>
+      );
+   }
+
    if (props.campsite) {
       return (
          <div className="container">
@@ -142,10 +164,10 @@ function CampsiteInfo(props) {
             </div>
             <div className="row">
                <RenderCampsite campsite={props.campsite} />
-               <RenderComments 
-               comments={props.comments}
-               addComment={props.addComment}
-               campsiteId={props.campsite.id} />
+               <RenderComments
+                  comments={props.comments}
+                  addComment={props.addComment}
+                  campsiteId={props.campsite.id} />
             </div>
          </div>
       );
