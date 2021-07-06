430c349934d5c2a334e07c02eb625461b0160dae
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-07-06 15:56:31 -0400

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,30 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import {createStore, combineReducers} from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+//import { Reducer, initialState } from './reducer';
 // ^ the const we export in reducer file
+
+// export const ConfigureStore = () => {
+//     const store = createStore(
+//         Reducer,
+//         initialState
+//     );
+
+//     return store;
+// };
+
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions
+        })
     );
 
     return store;
